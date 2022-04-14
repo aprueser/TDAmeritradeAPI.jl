6c99d4bba7382853412df2a94b4af9825f1334a0
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 02:35:46 +0000

    build based on c745ee0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TDAmeritradeAPI","category":"page"},{"location":"#TDAmeritradeAPI","page":"Home","title":"TDAmeritradeAPI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TDAmeritradeAPI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TDAmeritradeAPI]","category":"page"}]
+}
